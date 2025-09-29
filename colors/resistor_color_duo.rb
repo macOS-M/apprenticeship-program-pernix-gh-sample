82d792c388dcd67fbe1b034cfe4917c160854cba
Marcos Morales <mmorales99c@gmail.com>
2025-09-29 09:22:20 -0600

    Completed all day 8 ruby tests

---
+++ colors/resistor_color_duo.rb
@@ -0,0 +1,27 @@
+=begin
+Write your code for the 'Resistor Color Duo' exercise in this file. Make the tests in
+`resistor_color_duo_test.rb` pass.
+
+To get started with TDD, see the `README.md` file in your
+`ruby/resistor-color-duo` directory.
+=end
+class ResistorColorDuo
+  COLOR_CODES = {
+    "black" => 0,
+    "brown" => 1,
+    "red" => 2,
+    "orange" => 3,
+    "yellow" => 4,
+    "green" => 5,
+    "blue" => 6,
+    "violet" => 7,
+    "grey" => 8,
+    "white" => 9
+  }
+
+  def self.value(colors)
+    first_two = colors.first(2)
+    digits = first_two.map { |color| COLOR_CODES[color] }
+    digits.join.to_i
+  end
+end
